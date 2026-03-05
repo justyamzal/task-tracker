55fb3c269e78abbf684a87be5680a75c2cf8d77b
justyamzal <justyamzal@gmail.com>
2026-03-05 18:51:39 +0700

    add routes api

---
+++ backend/routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProjectController;
+use App\Http\Controllers\Api\TaskController;
+use App\Http\Controllers\Api\DashboardController;
+use App\Http\Controllers\Api\CategoryController;
+
+Route::post('/auth/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
+
+    Route::get('/dashboard/summary', [DashboardController::class, 'summary']);
+
+    Route::get('/categories', [CategoryController::class, 'index']);
+
+    Route::get('/projects', [ProjectController::class, 'index']);
+    Route::post('/projects', [ProjectController::class, 'store']);
+    Route::get('/projects/{project}', [ProjectController::class, 'show']);
+    Route::put('/projects/{project}', [ProjectController::class, 'update']);
+    Route::patch('/projects/{project}', [ProjectController::class, 'update']);
+
+    Route::get('/tasks', [TaskController::class, 'index']);
+    Route::post('/tasks', [TaskController::class, 'store']);
+    Route::put('/tasks/{task}', [TaskController::class, 'update']);
+    Route::patch('/tasks/{task}/move', [TaskController::class, 'move']);
+    Route::delete('/tasks/{task}', [TaskController::class, 'destroy']);
+});
